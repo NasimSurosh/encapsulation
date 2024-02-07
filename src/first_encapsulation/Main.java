9b4a7955e8c2284d3e8a78c630de188ffc544536
NasimSursh <suroshzahiri@gmail.com>
2024-02-07 10:56:41 +0100

    encapsulation

---
+++ src/first_encapsulation/Main.java
@@ -0,0 +1,15 @@
+package first_encapsulation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		first student = new first();
+		
+		student.setName("Kamil");
+		System.out.println(student.getName());
+	
+
+	}
+
+}
